b69a6c5a795ac9b205c8a26c7a486c85c497ab6d
Anubhav0905 <160308311+Anubhav0905@users.noreply.github.com>
2024-10-22 00:21:02 +0530

    Update ContextReducer.js

--- src/Components/ContextReducer.js
+++ src/Components/ContextReducer.js
@@ -20,15 +20,6 @@ const reducer = (state, action) =>{
             newArr.splice(action.index, 1)
             return newArr;
 
-        // case "UPDATE" :
-        //     let arr = [...state]
-        //     arr.find((food,index) => {
-        //         if(food.id === action.id){
-        //             console.log(food.qty, parseInt(action.qty),action.price + food.price)
-        //             arr[index] = {...food, qty: parseInt(action.qty) + food.qty, price: action}
-        //         }
-        //     })
-
             default:
                 console.log("Error in Reducer");
     }
